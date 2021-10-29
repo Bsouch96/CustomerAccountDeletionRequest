844427a09b2fae7e9b413255ec41202e8d30cee3
bsouch96 <v8178742@live.tees.ac.uk>
2021-10-29 18:00:14 +0100

    Implemented the Deletion Request DbContextand registered it in the Startup class. Created a new connection string key but awaiting value in appsettings.

---
+++ Context/CustomerAccountDeletionRequestContext.cs
@@ -0,0 +1,20 @@
+﻿using CustomerAccountDeletionRequest.DomainModels;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerAccountDeletionRequest.Context
+{
+    public class CustomerAccountDeletionRequestContext : DbContext
+    {
+        public CustomerAccountDeletionRequestContext(DbContextOptions<CustomerAccountDeletionRequestContext>
+            options) : base(options)
+        {
+
+        }
+
+        public DbSet<DeletionRequestModel> DeletionRequestContext { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,9 +1,11 @@
+using CustomerAccountDeletionRequest.Context;
 using CustomerAccountDeletionRequest.Repositories.Concrete;
 using CustomerAccountDeletionRequest.Repositories.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -31,6 +33,8 @@ namespace CustomerAccountDeletionRequest
 
             //services.AddScoped(ICustomerAccountDeletionRequestRepository, FakeCustomerAccountDeletionRequestRepository);
             services.AddScoped<ICustomerAccountDeletionRequestRepository, FakeCustomerAccountDeletionRequestRepository>();
+            services.AddDbContext<CustomerAccountDeletionRequestContext>(options => options.UseSqlServer
+            (Configuration.GetConnectionString("CustomerAccountDeletionRequestConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
