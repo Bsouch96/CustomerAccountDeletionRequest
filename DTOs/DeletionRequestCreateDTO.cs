2631f4d2d5497dbca11e1cabfaea7e17836a1cfe
bsouch96 <v8178742@live.tees.ac.uk>
2021-10-29 19:12:24 +0100

    Creation of the DeletionRequesstCreateDTO DTO.

---
+++ DTOs/DeletionRequestCreateDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerAccountDeletionRequest.DTOs
+{
+    public class DeletionRequestCreateDTO
+    {
+        public string CustomerName { get; set; }
+        public string DeletionReason { get; set; }
+    }
+}
