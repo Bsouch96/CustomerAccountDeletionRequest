3fbeb9863e8da7ebc7219e24459c442cc7a12e2e
bsouch96 <v8178742@live.tees.ac.uk>
2021-10-29 19:20:51 +0100

    Implemented the DeletionReqestProfile to enable Automapper to map between the DeletionRequestModel and the DeletionRequestCreateDTO

---
+++ Profiles/DeletionRequestProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using CustomerAccountDeletionRequest.DomainModels;
+using CustomerAccountDeletionRequest.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerAccountDeletionRequest.Profiles
+{
+    public class DeletionRequestProfile : Profile
+    {
+        public DeletionRequestProfile()
+        {
+            CreateMap<DeletionRequestModel, DeletionRequestCreateDTO>();
+        }
+    }
+}
