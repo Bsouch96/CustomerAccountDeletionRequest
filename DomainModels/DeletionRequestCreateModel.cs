ddc3da34eec1087afbe50a1294692718499f2f13
bsouch96 <v8178742@live.tees.ac.uk>
2021-10-29 16:51:32 +0100

    Implemented the create deletion request model and the deletion request status enum.

---
+++ DomainModels/DeletionRequestCreateModel.cs
@@ -0,0 +1,12 @@
+﻿using CustomerAccountDeletionRequest.Enums;
+
+namespace CustomerAccountDeletionRequest.DomainModels
+{
+    public class DeletionRequestCreateModel
+    {
+        public int CustomerID { get; set; }
+        public string CustomerName { get; set; }
+        public string DeletionReason { get; set; }
+        public DeletionRequestStatusEnum DeletionRequestStatus { get; set; }
+    }
+}
