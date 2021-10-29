cd60c89b1086aec8f114a6d181636f272de5b2f8
bsouch96 <v8178742@live.tees.ac.uk>
2021-10-29 18:15:48 +0100

    Applied data annotations to the Deletion Request domain model.

--- DomainModels/DeletionRequestModel.cs
+++ DomainModels/DeletionRequestModel.cs
@@ -1,11 +1,17 @@
 ﻿using CustomerAccountDeletionRequest.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace CustomerAccountDeletionRequest.DomainModels
 {
     public class DeletionRequestModel
     {
+        [Key]
         public int CustomerID { get; set; }
+        [Required]
+        [MaxLength(30)]
         public string CustomerName { get; set; }
+        [Required]
+        [MaxLength(300)]
         public string DeletionReason { get; set; }
         public DeletionRequestStatusEnum DeletionRequestStatus { get; set; }
     }
