cf1674c1c723dfbcb3a50be602d54146eb33c21a
bsouch96 <v8178742@live.tees.ac.uk>
2021-10-30 11:27:47 +0100

    Removed required annotation from DeletionReason in the DeletionRequestModel.

--- DomainModels/DeletionRequestModel.cs
+++ DomainModels/DeletionRequestModel.cs
@@ -7,12 +7,13 @@ namespace CustomerAccountDeletionRequest.DomainModels
     {
         [Key]
         public int CustomerID { get; set; }
+
         [Required]
         [MaxLength(30)]
         public string CustomerName { get; set; }
-        [Required]
-        [MaxLength(300)]
+
         public string DeletionReason { get; set; }
+
         public DeletionRequestStatusEnum DeletionRequestStatus { get; set; }
     }
 }
